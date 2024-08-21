ee2b110469fcbf05268e68d60e508c01b9da577f
devkavin <kavindra.senanayake@gmail.com>
2024-08-22 01:53:27 +0530

    Refactor AdminController to add delete user functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -37,4 +37,84 @@ class AdminController extends Controller
             'admins' => $adminResource
         ], 200);
     }
+
+    /**
+     * Delete an admin
+     *
+     * @param ViewInstructorsRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function deleteUser(Request $request)
+    {
+
+        $validated = $request->validate([
+            'id' => 'required|exists:users,id',
+        ]);
+
+
+        if ($validated['id'] === Auth::id()) {
+            return response()->json([
+                'message' => 'You cannot delete yourself'
+            ], 400);
+        }
+
+        $user = User::find($validated['id']);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+        if ($user->hasRole('admin')) {
+            return response()->json([
+                'message' => 'You cannot delete an admin'
+            ], 400);
+        }
+        if ($user->id === Auth::id()) {
+            return response()->json([
+                'message' => 'You cannot delete yourself'
+            ], 400);
+        }
+
+        $user->enrolledCourses()->detach(); // Unenroll the user from all courses
+        $user->createdCourses()->delete(); // Delete the courses created by the user
+        $user->delete(); // Delete the user
+        return response()->json([
+            'message' => 'User deleted successfully'
+        ], 200);
+    }
+
+    /**
+     * Bulk delete users
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function bulkDeleteUsers(Request $request)
+    {
+        $validated = $request->validate([
+            'ids' => 'required|array|min:1|exists:users,id',
+        ]);
+
+        $ids = $validated['ids'];
+        if (!is_array($ids)) {
+            return response()->json([
+                'message' => 'Invalid input'
+            ], 400);
+        }
+
+        if (in_array(Auth::id(), $ids)) {
+            return response()->json([
+                'message' => 'You cannot delete yourself'
+            ], 400);
+        }
+
+        User::whereIn('id', $ids)->each(function ($user) {
+            $user->enrolledCourses()->detach(); // Unenroll the user from all courses
+            $user->createdCourses()->delete(); // Delete the courses created by the user
+            $user->delete(); // Delete the user
+        });
+        return response()->json([
+            'message' => 'Users deleted successfully'
+        ], 200);
+    }
 }
